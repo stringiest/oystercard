c2b8262fffd735e42b8ad76865f0269b951c5c52
Lucy Stringer <lucyjstringer@gmail.com>
2020-11-03 16:47:37 +0000

    add in journey status and associated tests - step 8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -14,4 +14,16 @@ class Oystercard
   def deduct(fare)
     @balance -= fare
   end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
+  end
+
+  def in_journey?
+    @in_journey
+  end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -34,4 +34,24 @@ describe Oystercard do
       expect { subject.deduct(5) }.to change { subject.balance }.by -5
     end
   end
+
+  describe "#in_journey?" do
+
+    it 'is initially not in a journey' do
+      expect(subject).not_to be_in_journey
+    end
+
+    it "can touch in" do
+      subject.touch_in
+      expect(subject).to be_in_journey
+    end
+
+    it "can touch out" do
+      subject.touch_in
+      subject.touch_out
+      expect(subject).not_to be_in_journey
+    end
+
+  end
+
 end
